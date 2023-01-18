b0fa64756ec3e1d6065d473d264b155e5911515d
sreekanth.pogula <sreekanth.pogula@senecaglobal.com>
2023-01-18 11:21:12 +0530

    Added the modified code files

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,44 +1,30 @@
 import sqlite3
-import click
-from flask import current_app, g
+
+import pytest
+from restaurant.db import get_db
 
 
-def get_db():
-    if 'db' not in g:
-        g.db = sqlite3.connect(
-            current_app.config['DATABASE'],
-            detect_types=sqlite3.PARSE_DECLTYPES
-        )
-        g.db.row_factory = sqlite3.Row
+def test_get_close_db(app):
+    with app.app_context():
+        db = get_db()
+        assert db is get_db()
 
-    return g.db
+    with pytest.raises(sqlite3.ProgrammingError) as e:
+        db.execute('SELECT *')
 
-
-def close_db(e=None):
-    db = g.pop('db', None)
-
-    if db is not None:
-        db.close()
+    assert 'closed' in str(e.value)
 
 
-def init_db():
-    db = get_db()
+def test_init_db_command(runner, monkeypatch):
+    class Recorder(object):
+        called = False
 
-    with current_app.open_resource('schema.sql') as f:
-        db.executescript(f.read().decode('utf8'))
-
-
-@click.command('init-db')
-def init_db_command():
-    """Clear the existing data and create new tables."""
-    init_db()
-    click.echo('Initialized the database.')
-
+    def fake_init_db():
+        Recorder.called = True
 
 def init_app(app):
     app.teardown_appcontext(close_db)
     app.cli.add_command(init_db_command)
-
 
 def test_db():
     conn = get_db()
